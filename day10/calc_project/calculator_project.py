65ca27a14d01f5eefd5ef7f1bbfa6127bab2895a
NiyaziPro <niyazicholak@gmail.com>
2025-07-04 21:58:32 +0200

    21:58 Friday 04.07.2025

--- day10/calc_project/calculator_project.py
+++ day10/calc_project/calculator_project.py
@@ -1,7 +1,6 @@
 from day10.calc_project import calc_art
 from utils import colors
 
-print(calc_art.logo)
 
 def add(n1, n2):
     return n1 + n2
@@ -18,7 +17,7 @@ def multiply(n1, n2):
 def divide(n1, n2):
     if n2 == 0:
         print(colors.red + "You can not divide by zero! Please select other number!" + colors.reset)
-        return None
+        return 0
     else:
         return n1 / n2
 
@@ -30,8 +29,35 @@ operations = {
     "/": divide,
 }
 
-first_number = float(input("What's the first number?\n"))
-operation = input("+\n-\n*\n/\nPick an operation: ")
-next_number = float(input("What's the next number?\n"))
 
-print(operations.get("+")(first_number,next_number))
+def calculator():
+    print(calc_art.logo)
+    should_accumulate = True
+    first_number = float(input("What's the first number?\n"))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation = input("Pick an operation: ")
+        if operation == "+" or operation == "-" or operation == "*" or operation == "/":
+            should_accumulate = True
+        else:
+            should_accumulate = False
+            print(colors.red + "Invalid Operator!" + colors.reset)
+            calculator()
+        next_number = float(input("What's the next number?\n"))
+
+        answer = operations[operation](first_number, next_number)
+
+        print(f"{first_number} {operation} {next_number} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            first_number = answer
+        else:
+            should_accumulate = False
+            calculator()
+
+
+calculator()
