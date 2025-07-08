44cef62f2c18b0a60b84d2f2bf347830e4131b48
NiyaziPro <niyazicholak@gmail.com>
2025-07-08 19:24:49 +0200

    19:24 Tuesday 08.07.2025

---
+++ day14/game_data.py
@@ -0,0 +1,122 @@
+data = [
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 630,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Leo Messi',
+        'follower_count': 520,
+        'description': 'Footballer',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Dua Lipa',
+        'follower_count': 95,
+        'description': 'Singer and songwriter',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Zlatan Ibrahimović',
+        'follower_count': 60,
+        'description': 'Footballer',
+        'country': 'Sweden'
+    },
+    {
+        'name': 'FC Barcelona',
+        'follower_count': 120,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Adele',
+        'follower_count': 80,
+        'description': 'Singer',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Louis Vuitton',
+        'follower_count': 60,
+        'description': 'Luxury fashion brand',
+        'country': 'France'
+    },
+    {
+        'name': 'BMW',
+        'follower_count': 40,
+        'description': 'Automobile manufacturer',
+        'country': 'Germany'
+    },
+    {
+        'name': 'Kylian Mbappé',
+        'follower_count': 120,
+        'description': 'Footballer',
+        'country': 'France'
+    },
+    {
+        'name': 'Gordon Ramsay',
+        'follower_count': 35,
+        'description': 'Celebrity chef',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Camila Cabello',
+        'follower_count': 65,
+        'description': 'Singer',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Ryanair',
+        'follower_count': 1,
+        'description': 'Low-cost airline',
+        'country': 'Ireland'
+    },
+    {
+        'name': 'Burberry',
+        'follower_count': 18,
+        'description': 'Luxury fashion brand',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Zara',
+        'follower_count': 55,
+        'description': 'Clothing retailer',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Heidi Klum',
+        'follower_count': 10,
+        'description': 'Model and TV host',
+        'country': 'Germany'
+    },
+    {
+        'name': 'Vogue France',
+        'follower_count': 8,
+        'description': 'Fashion magazine',
+        'country': 'France'
+    },
+    {
+        'name': 'Erling Haaland',
+        'follower_count': 50,
+        'description': 'Footballer',
+        'country': 'Norway'
+    },
+    {
+        'name': 'Novak Djokovic',
+        'follower_count': 15,
+        'description': 'Tennis player',
+        'country': 'Serbia'
+    },
+    {
+        'name': 'Greta Thunberg',
+        'follower_count': 13,
+        'description': 'Climate activist',
+        'country': 'Sweden'
+    },
+    {
+        'name': 'Rosalía',
+        'follower_count': 25,
+        'description': 'Singer',
+        'country': 'Spain'
+    }
+]
