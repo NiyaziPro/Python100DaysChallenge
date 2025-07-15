bbc11e5bd096f69f555ab2882ed7f6acecc2e9bb
NiyaziPro <niyazicholak@gmail.com>
2025-07-15 19:17:23 +0200

    19:17 Tuesday 15.07.2025

---
+++ day18/turtle_graphics.py
@@ -0,0 +1,31 @@
+# https://docs.python.org/3/library/turtle.html
+# https://trinket.io/docs/colors
+# https://cs111.wellesley.edu/reference/colors
+
+from turtle import *
+
+timmy = Turtle()
+
+timmy.shape("turtle")
+timmy.color("green")
+# timmy.forward(100)
+# timmy.backward(200)
+# timmy.right(90)
+# timmy.left(180)
+# timmy.setheading(0)
+
+for line in range(4):
+    timmy.forward(100)
+    timmy.left(90)
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
