9ef9b4d395becb1ad151304af63c85245b16ed21
NiyaziPro <niyazicholak@gmail.com>
2025-07-24 20:17:37 +0200

    20:17 Thursday 24.07.2025

--- day20/snake/main.py
+++ day20/snake/main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
 
 from day20.snake.snake import Snake
 
@@ -9,20 +9,12 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-# segment_1 = Turtle(shape="square")
-# segment_1.color("white")
-#
-# segment_2 = Turtle(shape="square")
-# segment_2.color("white")
-# segment_2.goto(-20, 0)
-#
-# segment_3 = Turtle(shape="square")
-# segment_3.color("white")
-# segment_3.goto(-40, 0)
-
-# https://docs.python.org/3.3/library/turtle.html?highlight=turtle#turtle.tracer
-
 snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
@@ -30,6 +22,5 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
-
 
 screen.exitonclick()
