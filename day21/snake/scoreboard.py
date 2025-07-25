41824df4f06b6c6eb39894f2e36cd5746a46c562
NiyaziPro <niyazicholak@gmail.com>
2025-07-25 19:27:26 +0200

    19:27 Friday 25.07.2025

--- day21/snake/scoreboard.py
+++ day21/snake/scoreboard.py
@@ -18,8 +18,8 @@ class Scoreboard(Turtle):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over",align=ALIGNMENT,font=FONT)
+        self.goto(0, 0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
