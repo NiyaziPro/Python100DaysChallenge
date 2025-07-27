8f9d6d056b0a96ba47483cf341e530b71cec41f1
NiyaziPro <niyazicholak@gmail.com>
2025-07-27 23:03:23 +0200

    23:03 Sunday 27.07.2025

--- day22/main.py
+++ day22/main.py
@@ -3,6 +3,7 @@ from turtle import Screen, Turtle
 
 from day22.ball import Ball
 from day22.paddle import Paddle
+from day22.scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -10,10 +11,10 @@ screen.setup(height=600, width=800)
 screen.title("Pong")
 screen.tracer(0)
 
-r_paddle = Paddle((350,0))
-l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -24,12 +25,25 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision wit paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect when paddle missed
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
