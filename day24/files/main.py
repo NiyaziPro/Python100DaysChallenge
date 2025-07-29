82d21bb3ee87dc0cb7b1f84e473d99daf1db27fc
NiyaziPro <niyazicholak@gmail.com>
2025-07-29 19:02:22 +0200

    19:02 Tuesday 29.07.2025

--- day24/files/main.py
+++ day24/files/main.py
@@ -7,3 +7,8 @@ file.close()
 
 with open("new_file.txt", mode="w") as file:
     file.write("\nCreating new file")
+
+
+with open("/Users/TechPro/Desktop/New_file.txt") as new_file:
+    contents = new_file.read()
+    print(contents)
