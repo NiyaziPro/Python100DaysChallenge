79c78f14b23d5386f01b4a209cbb49d3ab904953
NiyaziPro <niyazicholak@gmail.com>
2025-08-04 21:10:50 +0200

    21:10 Monday 04.08.2025

--- day28/pomodoro-start/main.py
+++ day28/pomodoro-start/main.py
@@ -7,7 +7,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
@@ -21,6 +21,8 @@ def reset_timer():
     canvas.itemconfig(timer_text, text="00:00")
     title_label.config(text="Timer")
     check_marks.config(text="")
+    global reps
+    reps = 0
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
@@ -60,7 +62,7 @@ def count_down(count):
         marks = ""
         work_sessions = floor(reps / 2)
         for _ in range(work_sessions):
-            marks += "✓"
+            marks += "✔"
             check_marks.config(text=marks)
 
 
@@ -70,21 +72,22 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW, highlightthickness=0)
+title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 image_t = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=image_t)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(column=1, row=1)
 
-title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW, highlightthickness=0)
-title_label.grid(column=1, row=0)
 
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(fg=GREEN, background=YELLOW, font=(FONT_NAME, 20, "bold"))
+check_marks = Label(fg=GREEN, background=YELLOW)
 check_marks.grid(column=1, row=3)
 
 window.mainloop()
