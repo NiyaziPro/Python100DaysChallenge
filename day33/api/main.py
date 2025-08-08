79f16f6237d45681226c35cfb588fb8a67738c3d
NiyaziPro <niyazicholak@gmail.com>
2025-08-08 22:45:47 +0200

    22:45 Friday 08.08.2025

--- day33/api/main.py
+++ day33/api/main.py
@@ -1,6 +1,58 @@
-import requests
+import os
+import smtplib
+import time
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-print(response.json())
-print(response.json()["iss_position"]["longitude"])
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+MY_EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+MY_LAT = 53.865467
+MY_LONG = 10.686559
+
+
+def is_iss_overhead():
+    response_iss = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response_iss.raise_for_status()
+    data_iss = response_iss.json()
+
+    iss_latitude = float(data_iss["iss_position"]["latitude"])
+    iss_longitude = float(data_iss["iss_position"]["longitude"])
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG - 5:
+        return True
+    return False
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters, )
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+    return False
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg="Subject:Look Up\n\nThe ISS is above you in the sky!")
+            connection.close()
