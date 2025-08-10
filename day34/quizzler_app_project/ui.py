5b435f3689587bd4528a7ca47456b69cce27b16c
NiyaziPro <niyazicholak@gmail.com>
2025-08-10 20:10:27 +0200

    20:10 Sunday 10.08.2025

--- day34/quizzler_app_project/ui.py
+++ day34/quizzler_app_project/ui.py
@@ -1,12 +1,12 @@
 from tkinter import *
-
-from utils.colors import bg_red
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -15,7 +15,7 @@ class QuizInterface:
         self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150, 125, text="Some Question Text", fill=THEME_COLOR,
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="Some Question Text", fill=THEME_COLOR,
                                                      font=("Arial", 20, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
@@ -27,4 +27,10 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
