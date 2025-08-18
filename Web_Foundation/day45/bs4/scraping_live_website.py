f8301e5ab81384e45119a11f92536f323aae9949
NiyaziPro <niyazicholak@gmail.com>
2025-08-18 18:48:01 +0200

    18:48 Monday 18.08.2025

--- Web_Foundation/day45/bs4/scraping_live_website.py
+++ Web_Foundation/day45/bs4/scraping_live_website.py
@@ -5,10 +5,32 @@ response = requests.get("https://news.ycombinator.com/news")
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
-article_tag = soup.select_one("span.titleline a")
-article_text = article_tag.getText()
-article_link = article_tag.get("href")
-article_upvote = soup.find(name="span", class_="score").getText()
-print(article_text)
-print(article_link)
-print(article_upvote)
+# article_tag = soup.select_one("span.titleline a")
+# article_text = article_tag.getText()
+# article_link = article_tag.get("href")
+# article_upvote = soup.find(name="span", class_="score").getText()
+# print(article_text)
+# print(article_link)
+# print(article_upvote)
+
+articles = soup.find_all(name="span", class_="titleline")
+article_texts = []
+article_links = []
+
+for artticle_tag in articles:
+    text = artticle_tag.find("a").getText()
+    article_texts.append(text)
+    link = artticle_tag.find("a").get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
+
+print(article_texts)
+print(article_links)
+print(article_upvotes)
